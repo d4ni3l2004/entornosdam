3083ef416cab8dd4a82503eb3bce97d57ae742e5
asir22-13 <asir22-13@c16x03.iescierva.net>
2024-01-30 09:27:34 +0100

    EDT2 Mostrar mi nombre en un mensaje

--- Ejemplo1.java
+++ Ejemplo1.java
@@ -1,5 +1,7 @@
 public class Ejemplo1 {
 	public static void main(String[] args) {
-		System.out.println("HOLA MUNDO");	
+		System.out.println("HOLA MUNDO");
+		String nombre = "Daniel";
+		System.out.println("Mi nombre es " + nombre);
 	}
 }
